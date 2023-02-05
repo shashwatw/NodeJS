91fa20439c030ee2d013f8859d322c72d8d6f9c9
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-05 12:09:24 +0530

    added Prettier config

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -42,48 +42,52 @@ console.log("will read file");
 //////////////////////////////////////
 //Server
 
-
-
 //synchronous verison in next 3 lines because it is top level of code and is executed only once
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj =  JSON.parse(data);
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-const server = http.createServer((req, res)=>{
-    const { query, pathname } = url.parse(req.url,true);
-
-
-//overview page
-if(pathname === '/' || pathname === '/overview')
-{
+  //overview page
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-        'Content-type': 'text/html'});
+      'Content-type': 'text/html',
+    });
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
-
-}
-//product page
-else if(pathname === '/product')
-{
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  }
+  //product page
+  else if (pathname === '/product') {
     res.writeHead(200, {
-        'Content-type': 'text/html'});
+      'Content-type': 'text/html',
+    });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
-
-}
-//API
-else if(pathname === '/api')
-{
+  }
+  //API
+  else if (pathname === '/api') {
     // fs.readFile('./dev-data/data.json')
     /* no need of this code as it is already executed in top level of code
     fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err,data)=>{
@@ -91,21 +95,21 @@ else if(pathname === '/api')
     */
     //    console.log(productData);
     res.writeHead(200, {
-        'Content-type': 'application/json'});
-       res.end(data);
-}
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+  }
 
-//NOT FOUND
-else{
+  //NOT FOUND
+  else {
     res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world'
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
     res.end('<h1>Page not found !</h1>');
-}
+  }
 });
 
-server.listen(8000, '127.0.0.1', ()=>{
-    console.log('Listening to requests on port 8000');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
-
