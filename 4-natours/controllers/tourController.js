cdf066f9b4fa74f8518a893fd43717ebc92a69c6
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-17 22:03:23 +0530

    Reading using mongoose cRud

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,32 +1,40 @@
 const Tour = require("./../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  const id = req.params.id * 1;
-
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: "success",
-  // results: tours.length,
-  // data: {
-  //   tour,
-  // },
-  //   });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -34,6 +42,7 @@ exports.createTour = async (req, res) => {
     //const newTour = new Tour({});
     //newTour.save();
     const newTour = await Tour.create(req.body);
+    //Tour.findOne({ _id: req.params.id })
 
     res.status(201).json({
       status: "success",
