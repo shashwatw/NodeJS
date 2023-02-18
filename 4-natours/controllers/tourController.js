dad1125cb0f59d983c39f59a62ca404ad03e28d0
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-18 10:08:28 +0530

    API better filtering nd querying

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,8 +2,27 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // console.log(req.query, queryObj);
+
+    //simple query method(Build)
+    const query = Tour.find(queryObj);
+
+    //special mongoose query method
+    // const query = Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,
