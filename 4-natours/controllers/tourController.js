269ae9f01502a04aebbb9c1cf5dd96a7523c7449
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-18 10:22:24 +0530

    Advanced Filtering

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,15 +2,18 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     //BUILD QUERY
+
+    //1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // console.log(req.query, queryObj);
 
-    //simple query method(Build)
-    const query = Tour.find(queryObj);
+    //advanced query method(Build)
+    //1A) Filtering
 
     //special mongoose query method
     // const query = Tour.find()
@@ -18,6 +21,14 @@ exports.getAllTours = async (req, res) => {
     //   .equals(5)
     //   .where("difficulty")
     //   .equals("easy");
+
+    //Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    // {difficulty: 'easy', duration: {$gte: 5}}
+    //{ difficulty: 'easy', duration: { gte: '5' } }
+    const query = Tour.find(JSON.parse(queryStr));
 
     //EXECUTE QUERY
     const tours = await query;
