265a1c8b8063a35d0bdc7b6275cf3ba49d5f163a
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-19 10:40:58 +0530

    Refactoring API Features

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("./../utils/apiFeatures");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -9,68 +10,15 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    //BUILD QUERY
-
-    //1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // console.log(req.query, queryObj);
-
-    //advanced query method(Build)
-    //1A) Filtering
-
-    //special mongoose query method
-    // const query = Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
-
-    //1B Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-    // {difficulty: 'easy', duration: {$gte: 5}}
-    //{ difficulty: 'easy', duration: { gte: '5' } }
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      // console.log(sortBy);
-      query = query.sort(sortBy);
-      //sort('price ratingsAverage')
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    //3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v"); //EXCLUDES V FIELD IN RESPONSE
-    }
-
-    //4) Pagination
-
-    const page = req.query.page * 1 || 1; //converts string to number AND DEFINING DEFAULT PAGE
-    const limit = req.query.limit * 1 || 100; //converts string to number AND DEFINING DEFAULT LIMIT
-    const skip = (page - 1) * limit; //skip 1st 10 results, page 2 skip 10, page 3 skip 20
-
-    //pages=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exist");
-    }
+    // console.log(req.query);
 
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
